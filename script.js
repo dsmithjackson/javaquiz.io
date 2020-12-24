b57745d620fb9fb21dbf53e5b4c03225fe63f667
Dionne Smith-Jackson <dionnesmithjackson@gmail.com>
2020-12-23 23:56:18 -0500

    cleanup should be const

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-page1 = document.getElementById('page1');
-page2 = document.getElementById('page2');
-page3 = document.getElementById('page3');
+const page1 = document.getElementById('page1');
+const page2 = document.getElementById('page2');
+const page3 = document.getElementById('page3');
 
 let timer = 60;
 
